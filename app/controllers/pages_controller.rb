7af4de102d56e02edbe8ec264bc01af26dd10bbb
ismail <elfaqir963@gmail.com>
2016-02-17 18:41:45 +0100

    Chapitre 5

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+
+  def home
+	@title="Accueil"
+  end
+
+  def contact
+  end
+
+  def aide
+  end
+
+  def inscription
+  end
+
+  def cnx
+  end
+
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '69d36b488b1f66531e5809f9df37ac59388843260beedcf5e1ceb7cc264ada5dd721353ac6cdde28f29fabc79fddeaff9d2d5db27dda26e74adb0c2a7c189677'
